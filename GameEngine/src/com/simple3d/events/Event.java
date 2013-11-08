ebdc3251995cf47b1d4d0a39601182aa0c0e6c3e
Claira Safi <csafi@email.arizona.edu>
2013-11-08 15:41:22 -0700

    Test, please ignore : I

--- GameEngine/src/com/simple3d/events/Event.java
+++ GameEngine/src/com/simple3d/events/Event.java
@@ -2,14 +2,6 @@ package com.simple3d.events;
 
 import org.lwjgl.Sys;
 
-/**
- * Event is a superclass describing an event. All events extend Event. Events
- * are immutable by convention.
- * */
-/**
- * @author Chris
- *
- */
 public abstract class Event
 {
 	long timestamp;
