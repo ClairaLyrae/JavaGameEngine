3796ca51527d09b09bdf456a17f58f7125830901
wkrafft <wkrafft@gmail.com>
2013-12-07 12:41:59 -0700

    Added some stuff to AssetManager for sound. Added a SoundTest file. Added a laser.wav

---
+++ GameEngine/src/com/javagameengine/assets/audio/Sound.java
@@ -0,0 +1,17 @@
+package com.javagameengine.assets.audio;
+
+import java.io.File;
+
+public class Sound {
+
+	public static Sound loadFromFile(File f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
